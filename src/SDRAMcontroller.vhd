eceb19c2e7d52c92926d3adacfb8e56a3bd292e5
René Richard <rene@db-elec.com>
2016-02-11 15:58:57 -0500

    changed license in code to GPLv3

--- src/SDRAMcontroller.vhd
+++ src/SDRAMcontroller.vhd
@@ -1,33 +1,23 @@
--- Matthew Hagerty, copyright 2014
+--    Title:            SDRAMController.vhd
+--    Original Author:  Matthew Hagerty
+--	  Modified by: 		René Richard
+--    Description:
+--        
+-- LICENSE
+-- 
+--    This file is part of SDRAMController.
+--    TDSN76489 is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--    Foobar is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--    You should have received a copy of the GNU General Public License
+--    along with SDRAMController.  If not, see <http://www.gnu.org/licenses/>.
 --
--- Create Date: 18:22:00 March 18, 2014
--- Module Name: sdram_simple - RTL
---
--- Simple SDRAM Controller for Winbond W9812G6JH-75
--- Can synthesize: Yes
--- Proven in hardware: Yes
---
--- The MIT License (MIT)
---
--- Copyright (c) 2014 Matthew Hagerty
---
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
---
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
+
 
 library IEEE, UNISIM;
 use IEEE.std_logic_1164.all;
